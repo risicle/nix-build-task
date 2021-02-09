296265aaaf40fdf51c831d479169e6d298ed107d
Robert Scott <code@humanleg.org.uk>
2021-02-09 23:09:39 +0000

    use niv to manage nixpkgs base

--- default.nix
+++ default.nix
@@ -1,13 +1,11 @@
 {
-  pkgs ? import (builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/2b9daa020d40aac9d6ff3d1941d22acf4a3e9229.tar.gz";
-    sha256 = "0kh2h5cbyxijy9i0mzmfzvma0qnp9zj0p3lbmy5imw2429jdpnx7";
-  }) {}
+  pkgs ? import (import ./nix/sources.nix).nixpkgs {}
   , baseImage ? null
   , ...
 }:
 
 let
+  # TODO use a lighter-weight base image
   baseImage_ = if baseImage != null then baseImage else pkgs.dockerTools.pullImage {
     imageName = "nixos/nix";
     imageDigest = "sha256:a6bcef50c7ca82ca66965935a848c8c388beb78c9a5de3e3b3d4ea298c95c708";
@@ -39,4 +37,10 @@ in rec {
     config.Cmd = [ "/bin/build" ];
   };
   imageConcourse = nix-build-task-lib.concourseStyleImageOutput image;
+  bumpSources = pkgs.writeScript "bump-sources" ''
+    #!${pkgs.bash}/bin/bash
+    set -e
+
+    ${pkgs.niv}/bin/niv update nixpkgs
+  '';
 }
