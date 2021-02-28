90e70dcd0a911755b8b96e5a3c9d3154fa22f366
Robert Scott <code@humanleg.org.uk>
2021-02-28 17:01:42 +0000

    TMP

--- tests/other.nix
+++ tests/other.nix
@@ -10,12 +10,14 @@ rec {
     url = "https://kernel.org/pub/linux/kernel/v1.0/linux-1.0.tar.gz";
     sha256 = "00hqn0mdf3097f69zib8q6la8i8f1qaf6hxp7r46mnx3d7mc6k01";
   };
-  linux-dir = pkgs.linkFarm "linux-dir" [
-    {name = "README${readmeExt}"; path = pkgs.writeText "README" "Not a lot";}
-    {name = "linux_1_0"; path = pkgs.runCommand "linux_1_0" {} ''
-      mkdir $out
-      tar -C $out -zxf ${linux_1_0}
-    '';}
-  ] ++ pkgs.lib.optional includeTarball {name = "linux_1_0.tar.gz"; path = linux_1_0;};
+  linux-dir = pkgs.linkFarm "linux-dir" (
+    [
+      {name = "README${readmeExt}"; path = pkgs.writeText "README" "Not a lot";}
+      {name = "linux_1_0"; path = pkgs.runCommand "linux_1_0" {} ''
+        mkdir $out
+        tar -C $out -zxf ${linux_1_0}
+      '';}
+    ] ++ pkgs.lib.optional includeTarball {name = "linux_1_0.tar.gz"; path = linux_1_0;}
+  );
   skopeo = pkgs.skopeo;
 }
