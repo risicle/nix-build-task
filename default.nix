e53fa9e81b513a72a71af4a6d847764faee29d24
Robert Scott <code@humanleg.org.uk>
2024-07-06 16:32:48 +0100

    TMP

--- default.nix
+++ default.nix
@@ -17,7 +17,7 @@ rec {
       pkgs.runCommand "nix-build-task-build" {
         buildInputs = [ pkgs.python3 pkgs.makeWrapper ];
         wrappedPath = pkgs.lib.makeBinPath (with pkgs; [
-          haskellPackages.cachix
+          cachix
           gnutar
           gzip
           pkgs.nix
